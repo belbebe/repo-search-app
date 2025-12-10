becd31f8be3d8916b663c51e229389d0405d4c57
Belcsik Bence <belcsik.bebe09@gmail.com>
2025-12-10 21:02:08 +0100

    create project and add main screen

---
+++ RepoSearch/RepoSearch/Components/Modifiers/BorderWithCornerRadius.swift
@@ -0,0 +1,31 @@
+//
+//  BorderWithCornerRadius.swift
+//  RepoSearch
+//
+//  Created by Belcsik Bence on 2025. 12. 10..
+//
+
+import SwiftUI
+
+struct BorderWithCornerRadius: ViewModifier {
+    let backGroundColor: Color
+    let borderColor: Color
+    let cornerRadius: CGFloat
+    let borderWidth: CGFloat
+    
+    init(backGroundColor: Color, borderColor: Color, cornerRadius: CGFloat, borderWidth: CGFloat) {
+        self.backGroundColor = backGroundColor
+        self.borderColor = borderColor
+        self.cornerRadius = cornerRadius
+        self.borderWidth = borderWidth
+    }
+    
+    func body(content: Content) -> some View {
+        content
+            .background(backGroundColor)
+            .cornerRadius(cornerRadius)
+            .padding(borderWidth)
+            .background(borderColor)
+            .cornerRadius(cornerRadius + borderWidth)
+    }
+}
---
+++ RepoSearch/RepoSearch/RepoSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoSearchApp.swift
+//  RepoSearch
+//
+//  Created by Belcsik Bence on 2025. 12. 10..
+//
+
+import SwiftUI
+
+@main
+struct RepoSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
