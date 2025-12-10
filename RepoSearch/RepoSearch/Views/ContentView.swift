a77bd5326c58dca36370ba9908a15444be4bd03a
Belcsik Bence <belcsik.bebe09@gmail.com>
2025-12-10 21:02:08 +0100

    create project and add main screen

---
+++ RepoSearch/RepoSearch/Views/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  RepoSearch
+//
+//  Created by Belcsik Bence on 2025. 12. 10..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var stringToSearch = ""
+    var body: some View {
+        VStack {
+            TextField("Type here info about the repository...", text: $stringToSearch)
+                .padding(8)
+                .modifier(
+                    BorderWithCornerRadius(
+                        backGroundColor: Constants.mainBackgroundColor,
+                        borderColor: .gray,
+                        cornerRadius: 4,
+                        borderWidth: 1
+                    )
+                )
+                .padding(.horizontal, 16)
+                .padding(.bottom, 16)
+            
+            Button(action: {}) {
+                Text("Search")
+            }
+            .padding(8)
+            .foregroundStyle(.black)
+            .modifier(
+                BorderWithCornerRadius(
+                    backGroundColor: .blue,
+                    borderColor: .purple,
+                    cornerRadius: 8,
+                    borderWidth: 2
+                )
+            )
+        }
+        .padding()
+    }
+}
+
+// MARK: - Constants
+extension ContentView {
+    enum Constants {
+        static let mainBackgroundColor = Color("Main")
+    }
+}
+
+#Preview {
+    ContentView()
+}
